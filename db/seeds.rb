7b4fedd8b10a993a2c02dd65865517ab2f8144d1
Oskar Lakner <haerdde@gmail.com>
2020-07-01 22:57:38 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
 user = User.where(email: "user@example.com").first_or_create! do |u|
   u.password = 'password'
 end
-
-admin = User.where(email: "admin@example.com").first_or_create! do |u|
-  u.password = 'password'
-end
